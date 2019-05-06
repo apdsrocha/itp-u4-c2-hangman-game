16ec8cb9f0f8157ed167a8b2e28feed94fe30396
apdsrocha <apdsrocha@gmail.com>
2019-05-06 15:13:52 +0000

    passing final tests

--- hangman/game.py
+++ hangman/game.py
@@ -14,7 +14,6 @@ def _get_random_word(list_of_words):
     
     return chosen_word
 
-
 def _mask_word(word):
     num_of_asterix = len(word)
     if not word:
@@ -24,11 +23,65 @@ def _mask_word(word):
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if not answer_word or not masked_word:
+        raise InvalidWordException('Please provide word')
 
+    if len(character) > 1:
+        raise InvalidGuessedLetterException('Please provide guess')
+
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException('Incorrect info provided')
+
+    answer = answer_word.lower()
+    if character.lower() not in answer:
+        return masked_word
+
+    new_word = ''
+
+    for answer_char, masked_char in zip(answer, masked_word):
+        if character.lower() == answer_char:
+            new_word += answer_char
+        else:
+            new_word += masked_char
+
+    return new_word
+
+def _is_game_won(game):
+    return game['answer_word'].lower() == game['masked_word'].lower()
+
+
+def _is_game_lost(game):
+    return game['remaining_misses'] <= 0
+
+
+def _is_game_finished(game):
+    return _is_game_lost(game) or _is_game_won(game)
 
 def guess_letter(game, letter):
-    pass
+    letter = letter.lower()
+    if letter in game['previous_guesses']:
+        raise InvalidGuessedLetterException()
+
+    if _is_game_finished(game): 
+        raise GameFinishedException()
+
+    previous_masked = game['masked_word']
+    new_masked = _uncover_word(game['answer_word'], previous_masked, letter)
+
+    if previous_masked == new_masked:
+        game['remaining_misses'] -= 1
+    else:
+        game['masked_word'] = new_masked
+
+    game['previous_guesses'].append(letter)
+
+    if _is_game_won(game):
+        raise GameWonException()
+
+    if _is_game_lost(game):
+        raise GameLostException()
+
+    return new_masked
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
